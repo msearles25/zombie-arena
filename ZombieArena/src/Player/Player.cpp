59350d303a2afd29299fcb03f510f84f7c0d5aa0
msearles25 <skaman4459@gmail.com>
2020-10-30 18:14:37 -0400

    update function

--- ZombieArena/src/Player/Player.cpp
+++ ZombieArena/src/Player/Player.cpp
@@ -125,3 +125,56 @@ void Player::stopDown()
 {
 	m_DownPressed = false;
 }
+
+void Player::update(float elapsedTime, sf::Vector2i mousePosition)
+{
+	if (m_UpPressed)
+	{
+		m_Position.y -= m_Speed * elapsedTime;
+	}
+
+	if (m_DownPressed)
+	{
+		m_Position.y += m_Speed * elapsedTime;
+	}
+
+	if (m_RightPressed)
+	{
+		m_Position.x += m_Speed * elapsedTime;
+	}
+
+	if (m_LeftPressed)
+	{
+		m_Position.x -= m_Speed * elapsedTime;
+	}
+
+	m_Sprite.setPosition(m_Position);
+
+	// Keeping the player inside of the arena
+	if (m_Position.x > m_Arena.width - m_TileSize)
+	{
+		m_Position.x = m_Arena.width - m_TileSize;
+	}
+
+	if (m_Position.x < m_Arena.left + m_TileSize)
+	{
+		m_Position.x = m_Arena.left + m_TileSize;
+	}
+
+	if (m_Position.y > m_Arena.height - m_TileSize)
+	{
+		m_Position.y = m_Arena.height - m_TileSize;
+	}
+
+	if (m_Position.y < m_Arena.top + m_TileSize)
+	{
+		m_Position.y = m_Arena.top + m_TileSize;
+	}
+
+	// Calculate the angle that the player is facing
+	float angle{ (atan2(
+		mousePosition.y - m_Resolution.y / 2, 
+		mousePosition.x - m_Resolution.x / 2) * 180) / 3.141 };
+
+	m_Sprite.setRotation(angle);
+}

961aed807dd91c282d531a6610e83f0ef85b453c
msearles25 <skaman4459@gmail.com>
2020-10-30 23:06:33 -0400

    TextureHolder class

---
+++ ZombieArena/src/TextureHolder/TextureHolder.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef TEXTURE_HOLDER_H
+#define TEXTURE_HOLDER_H
+
+#include <map>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class TextureHolder
+{
+private:
+	// A map container from STL, that holds related pairs of String and Texture
+	std::map<std::string, sf::Texture> m_Textures;
+
+	// A pointer of the same type as the class itself, the one and only instance
+	static TextureHolder *m_s_Instance;
+public:
+	TextureHolder();
+	static sf::Texture& GetTexture(const std::string &filename);
+};
+
+#endif // TEXTURE_HOLDER_H
